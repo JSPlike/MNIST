6db0b009b37ea962d1219e35ab6036a69efe09af
JSPlike <moter2588@gmail.com>
2019-03-09 17:10:31 +0900

    gradient_descent 0309 05:10

---
+++ ch01/gradient_descent.py
@@ -0,0 +1,26 @@
+import tensorflow as tf
+import matplotlib.pyplot as plt
+
+W = tf.placeholder(tf.float32)
+
+X = [1, 2, 3]
+Y = [1, 2, 3]
+
+hypothesis = W * X
+
+cost = tf.reduce_mean(tf.square(hypothesis - Y))
+sess = tf.Session()
+sess.run(tf.global_variables_initializer())
+
+W_val = []
+cost_val = []
+for i in range(-30, 50):
+    feed_W = i * 0.1
+    curr_cost, curr_W = sess.run([cost, W], feed_dict={W: feed_W})
+    W_val.append(curr_W)
+    cost_val.append(curr_cost)
+
+plt.plot(W_val, cost_val)
+plt.show()
+
+print(tf.__version__)
